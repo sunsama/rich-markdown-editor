79926a776497259b383a9fac2c720485316e3260
Zane McCaig <zanemccaig@gmail.com>
2020-12-08 14:42:17 -0800

    Fixed issue where pasted text with url in it would be turned into link

--- src/lib/isUrl.ts
+++ src/lib/isUrl.ts
@@ -1,5 +1,6 @@
 export const URL_INPUT_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+export const STRICT_URL_INPUT_REGEX = /^(http(s)?:\/\/.)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g;
 
 export default function isUrl(text: string) {
-  return (text || "").match(URL_INPUT_REGEX);
+  return (text || "").match(STRICT_URL_INPUT_REGEX);
 }
