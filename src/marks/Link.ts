a778d0cc8ad2c72b51132908db97c784e73627b1
Zane McCaig <zanemccaig@gmail.com>
2020-12-08 16:17:28 -0800

    Fixed inputrule matching for links to match multiple links per line and to keep the full link content

--- src/marks/Link.ts
+++ src/marks/Link.ts
@@ -77,20 +77,17 @@ export default class Link extends Mark {
         return tr;
       }),
       new InputRule(URL_INPUT_REGEX, (state, match, start, end) => {
-        const isAlreadyLink = state.doc.rangeHasMark(start, end, type);
-        if (!isAlreadyLink) {
-          const [href] = match;
-          const { tr } = state;
-          if (href) {
-            tr.replaceWith(start, end, this.editor.schema.text(href)).addMark(
-              start,
-              start + href.length,
-              type.create({ href })
-            );
-          }
-          return tr;
+        const [href] = match;
+        const { tr } = state;
+
+        if (href) {
+          tr.replaceWith(start, end, this.editor.schema.text(href)).addMark(
+            start,
+            start + href.length,
+            type.create({ href })
+          );
         }
-        return null;
+        return tr;
       }),
     ];
   }
