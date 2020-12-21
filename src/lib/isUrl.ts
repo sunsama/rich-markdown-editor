1593f0746b120034ef686551b42f1bc29f60fb1e
Zane McCaig <zanemccaig@gmail.com>
2020-12-21 13:03:45 -0800

    Made the url regex a bit stronger

--- src/lib/isUrl.ts
+++ src/lib/isUrl.ts
@@ -1,4 +1,4 @@
-export const URL_INPUT_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{1,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+export const URL_INPUT_REGEX = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
 export const STRICT_URL_INPUT_REGEX = /^(http(s)?:\/\/.)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g;
 
 export default function isUrl(text: string) {
